526fcb0a21447746c9c6789c3141f731050ed594
cleverson <cleverson.s.dias@gmail.com>
2019-04-02 11:11:08 -0300

    Projeto Final

--- airbnb-web/src/routes.js
+++ airbnb-web/src/routes.js
@@ -1,36 +1,39 @@
-import React from 'react'
-import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
+import React, { Fragment } from "react";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import { ModalContainer } from "react-router-modal";
+import "react-router-modal/css/react-router-modal.css";
 
-import { isAuthenticated } from './services/auth'
+import { isAuthenticated } from "./services/auth";
 
-import App from './pages/App'
-import SignUp from './pages/SignUp'
-import SignIn from './pages/SignIn'
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import App from "./pages/App";
 
 const PrivateRoute = ({ component: Component, ...rest }) => (
-	<Route
-		{...rest}
-		render={props =>
-			isAuthenticated() ? (
-				<Component {...props} />
-			) : (
-				<Redirect
-					to={{ pathname: '/', state: { from: props.location } }}
-				/>
-			)
-		}
-	/>
-)
+  <Route
+    {...rest}
+    render={props =>
+      isAuthenticated() ? (
+        <Component {...props} />
+      ) : (
+        <Redirect to={{ pathname: "/", state: { from: props.location } }} />
+      )
+    }
+  />
+);
 
 const Routes = () => (
-	<BrowserRouter>
-		<Switch>
-			<Route exact path="/" component={SignIn} />
-			<Route path="/signup" component={SignUp} />
-			<PrivateRoute path="/app" component={App} />
-			<Route path="*" component={() => <h1>Page not found</h1>} />
-		</Switch>
-	</BrowserRouter>
-)
+  <BrowserRouter>
+    <Fragment>
+      <Switch>
+        <Route exact path="/" component={SignIn} />
+        <Route path="/signup" component={SignUp} />
+        <PrivateRoute path="/app" component={App} />
+        <Route path="*" component={() => <h1>Page not found</h1>} />
+      </Switch>
+      <ModalContainer />
+    </Fragment>
+  </BrowserRouter>
+);
 
-export default Routes
+export default Routes;
